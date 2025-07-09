21d9b9b69fddc9407e06116c23196724bd644b9a
Nicolas Talbot <talbotn123@gmail.com>
2025-07-09 16:44:17 -0400

    feat: add my/profile page to display user information

---
+++ pages/my/profile.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useProfileStore } from "~/stores/profile";
+const profileStore = useProfileStore();
+const user = useSupabaseUser();
+
+onMounted(async () => {
+    if (user.value) {
+        await profileStore.fetchProfileById(user.value.id);
+    }
+});
+</script>
+
+<template>
+    <div>
+        <h1>My Profile</h1>
+        <div v-if="profileStore.loading">Loading...</div>
+        <div v-else-if="profileStore.error" style="color: red">
+            {{ profileStore.error }}
+        </div>
+        <div v-else-if="profileStore.myProfile">
+            <p>
+                <strong>Username:</strong> {{ profileStore.myProfile.username }}
+            </p>
+            <p v-if="profileStore.myProfile.bio">
+                <strong>Bio:</strong> {{ profileStore.myProfile.bio }}
+            </p>
+            <img
+                v-if="profileStore.myProfile.avatar_url"
+                :src="profileStore.myProfile.avatar_url"
+                alt="Avatar"
+                style="max-width: 100px; max-height: 100px"
+            />
+        </div>
+        <div v-else>
+            <p>User not found.</p>
+        </div>
+    </div>
+</template>
