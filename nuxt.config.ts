9d6a3485b1ef9e536d98e6453e5301a7be2f76ee
Nicolas Talbot <talbotn123@gmail.com>
2025-07-10 23:16:24 -0400

    feat: add global styles and Sass support by creating variables and global SCSS files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2025-05-15",
-    devtools: { enabled: true },
-    modules: [
-      "@nuxt/eslint",
-      "@nuxtjs/supabase",
-      "@pinia/nuxt",
-      "@nuxtjs/tailwindcss",
-      "@nuxt/icon",
-    ],
-    supabase: {
-        url: process.env.SUPABASE_URL,
-        key: process.env.SUPABASE_ANON_KEY,
-        redirect: false,
-        redirectOptions: {
-            login: "/auth/login",
-            callback: "/auth/confirm",
-        },
-    },
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
+	modules: [
+		"@nuxt/eslint",
+		"@nuxtjs/supabase",
+		"@pinia/nuxt",
+		"@nuxtjs/tailwindcss",
+		"@nuxt/icon",
+	],
+	css: ["~/assets/css/global.scss"],
+	supabase: {
+		url: process.env.SUPABASE_URL,
+		key: process.env.SUPABASE_ANON_KEY,
+		redirect: false,
+		redirectOptions: {
+			login: "/auth/login",
+			callback: "/auth/confirm",
+		},
+	},
 });
