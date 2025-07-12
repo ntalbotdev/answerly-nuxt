86a6a17396fef1ee3019b6370467defe496fabdc
Nicolas Talbot <talbotn123@gmail.com>
2025-07-12 03:11:18 -0400

    feat: implement header component with navigation and user dropdown

---
+++ components/Header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { useProfileStore } from "~/stores/profile";
+import HeaderNavbar from "~/components/HeaderNavbar.vue";
+const user = useSupabaseUser();
+const profileStore = useProfileStore();
+
+// Profile fetch/clear logic remains here for now
+watchEffect(async () => {
+	if (user.value) {
+		if (
+			!profileStore.myProfile ||
+			profileStore.myProfile.user_id !== user.value.id
+		) {
+			await profileStore.fetchProfileById(user.value.id);
+		}
+	} else {
+		profileStore.clearProfile();
+	}
+});
+</script>
+
+<template>
+	<header class="header">
+		<div class="header__inner">
+			<div class="header__content">
+				<div class="header__logo">
+					<NuxtLink to="/">
+						<Icon
+							name="heroicons:chat-bubble-left-20-solid"
+							class="header__logo-icon"
+						/>
+						Answerly
+					</NuxtLink>
+				</div>
+
+				<HeaderNavbar />
+			</div>
+		</div>
+	</header>
+</template>
