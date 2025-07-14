0b08307388c335132aefb721ec5932eac6b573a7
Nicolas Talbot <talbotn123@gmail.com>
2025-07-14 06:30:08 -0400

    refactor: remove unused user authentication logic from global redirects middleware

--- middleware/redirects.global.ts
+++ middleware/redirects.global.ts
@@ -1,13 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
-    const user = useSupabaseUser();
-
-    // If not logged in, redirect /my to home
-    if (!user.value && (to.path === "/my" || to.path === "/my/")) {
-        return navigateTo("/");
-    }
-    // If logged in, redirect /my to /my/profile
-    else if (user.value && (to.path === "/my" || to.path === "/my/")) {
-        // If logged in, redirect /my to /my/profile
-        return navigateTo("/my/profile");
-    }
+    // const user = useSupabaseUser();
+    // if (!user.value && (to.path === "/my" || to.path === "/my/")) {
+    //     return navigateTo("/");
+    // }
 });
