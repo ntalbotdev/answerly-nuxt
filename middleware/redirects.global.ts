1061b592fa610a460651b61f543e133c0271c888
Nicolas Talbot <talbotn123@gmail.com>
2025-07-14 06:19:01 -0400

    refactor: renamed /my/profile to /profile and removed redirect

--- middleware/redirects.global.ts
+++ middleware/redirects.global.ts
@@ -1,9 +1,5 @@
 export default defineNuxtRouteMiddleware((to) => {
     const user = useSupabaseUser();
-    // Redirect non-existent /profile base route to home
-    if (to.path === "/profile" || to.path === "/profile/") {
-        return navigateTo("/");
-    }
 
     // If not logged in, redirect /my to home
     if (!user.value && (to.path === "/my" || to.path === "/my/")) {
