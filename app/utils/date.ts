d65cafb51e5263c3d9ef32a1db744ac47e8e8143
Nicolas Talbot <talbotn123@gmail.com>
2025-07-21 06:06:36 -0400

    fix: standardize date formatting to use 'en-GB' locale and ensure two-digit month and day

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,11 +1,12 @@
 export function formatDateNoSeconds(dateString: string) {
   const date = new Date(dateString);
-  return date.toLocaleString([], {
+  return date.toLocaleString('en-GB', {
     year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
-    hour12: true // or false for 24h
-  });
+    hour12: true, // or false for 24h
+  }).replace(',', ' at')
+  .replace(/\b(am|pm)\b/i, m => m.toUpperCase())
 }
