6f3a744ea5d0858183f99b3fc172cea621ba69cd
Nicolas Talbot <talbotn123@gmail.com>
2025-08-06 01:36:53 -0400

    refactor: redirect /profile to /profile/:username

--- app/middleware/redirects.global.ts
+++ app/middleware/redirects.global.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
-	// const user = useSupabaseUser();
-	// if (!user.value && (to.path === "/my" || to.path === "/my/")) {
-	//     return navigateTo("/");
-	// }
+	const user = useSupabaseUser();
+
+	if (user.value && (to.path === "/profile" || to.path === "/profile/")) {
+		const username = user.value.user_metadata?.username || user.value.id;
+		return navigateTo(`/profile/${username}`);
+	}
 });
