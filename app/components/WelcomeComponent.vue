6ce588cc761629356f1d482631ffb1491ff39042
Nicolas Talbot <talbotn123@gmail.com>
2025-08-07 02:14:42 -0400

    feat: add WelcomeComponent with initial template structure

---
+++ app/components/WelcomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="welcome">
+		<h2 class="welcome__title">
+			Ask. <span class="welcome__title--highlight">Answer.</span> Connect.
+		</h2>
+
+		<p class="welcome__description">
+			Unleash your curiosity—join the conversation.
+		</p>
+
+		<div class="welcome__buttons">
+			<NuxtLink
+				:to="ROUTES.SIGNUP"
+				class="welcome__button auth-form__button"
+			>
+				Sign up
+			</NuxtLink>
+
+			<NuxtLink
+				:to="ROUTES.LOGIN"
+				class="welcome__button auth-form__button auth-form__button--secondary"
+			>
+				Log in
+			</NuxtLink>
+		</div>
+	</div>
+</template>
